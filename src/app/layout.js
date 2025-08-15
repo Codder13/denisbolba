fb6efffb99281bc96ab082bbb2522925765836b0
Denis13 <denisbolba@gmail.com>
2025-08-16 00:12:18 +0300

    refactor: remove PlausibleProvider and integrate Umami script directly in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import PlausibleProvider from "next-plausible";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,24 +16,21 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 export default function RootLayout({ children }) {
   return (
-    <PlausibleProvider
-      domain="denisbolba.com"
-      customDomain="https://plausible.denisbolba.xyz"
-    >
-      <html lang="en" className="scroll-smooth">
-        {" "}
-        <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#1c1c1c" />
-          <link rel="icon" href="/favicon.ico" />
-        </head>{" "}
-        <body
-          className={`${inter.className} ${isDevelopment ? "dev-mode" : ""}`}
-        >
-          {children}
-          {isDevelopment && <div className="responsive-indicator" />}
-        </body>
-      </html>
-    </PlausibleProvider>
+    <html lang="en" className="scroll-smooth">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#1c1c1c" />
+        <link rel="icon" href="/favicon.ico" />
+        <Script
+          src="https://umami.denisbolba.xyz/script.js"
+          data-website-id="f0947877-6b8a-454a-b4b0-6af31493706b"
+          strategy="afterInteractive"
+        />
+      </head>
+      <body className={`${inter.className} ${isDevelopment ? "dev-mode" : ""}`}>
+        {children}
+        {isDevelopment && <div className="responsive-indicator" />}
+      </body>
+    </html>
   );
 }
